752cfee9e87ac7e248d44e2e13a8f9833991bcf8
KostasEreksonas <k.ereksonas@gmail.com>
2022-12-20 11:12:47 +0200

    Conversion of plain text to morse code

--- Javascript/js/converter.js
+++ Javascript/js/converter.js
@@ -37,13 +37,21 @@ const alphabet = {
     "0": "-----"
 }
 
-function getText() {
-    let result = document.getElementById("morseCode").value;
-    return result;
-}
+function convertText() {
+    let text = document.getElementById('morseCode').value;
+    for (let i = 0; i < text.length; i++) {
+        if (text[0] === '.' || text[0] === '-') {
+			document.getElementById('result').innerHTML = text + ' is written in morse code.';
+        } else {
+            let keys = Object.keys(alphabet);
+            for (let j = 0; j < keys.length; j++) {
+                if (text[i] === keys[j]) {
+                        document.getElementById('result').innerHTML += alphabet[keys[j]] + ' ';
+                    }
+                }
+            }
+		}
+    }
 
-function convertText(text) {
-    document.getElementById("result").innerHTML = text;
-}
-
-convertText(getText());
+let button = document.getElementById('convert');
+button.addEventListener('click', convertText);
